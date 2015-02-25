321c59a6d3736e117e1bf87fc8966e7f14ad9f84
neverbot <neverbot@neverbot.com>
2015-02-25 12:12:08 +0100

    Added forceDetach to the general Tracker class to force it detaching its items in their current positions

--- engine/code/behaviour/tracker.js
+++ engine/code/behaviour/tracker.js
@@ -33,3 +33,27 @@ Engine.Tracker.prototype.draw = function (ctx)
   Engine.Item.prototype.draw.call(this, ctx); 
 }
 
+Engine.Tracker.prototype.forceDetach = function()
+{
+  if (this.getParent() != null)
+  {
+    // Move all children from here to the parent
+    for (var i = 0, len = this.getAttachedItems().length; i < len; i++)
+    {
+      var element = this.getAttachedItems()[i];
+
+      element.position.x += this.position.x;
+      element.position.y += this.position.y;
+
+      // Exit speed, so the element does not stop
+      // element.speed.x = direction.x * this.trackSpeed;
+      // element.speed.y = direction.y * this.trackSpeed;
+
+      this.detachItem(element);
+      this.getParent().attachItem(element);
+    }
+
+    // Suicide!
+    this.getParent().detachItem(this);  
+  }
+}
