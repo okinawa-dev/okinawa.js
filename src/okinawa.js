19632b387934cd92d13affa90ca877db03e22b28
Iván Alonso <ivan@neverbot.com>
2019-11-16 10:42:36 +0100

    library entry point as single file with every export

--- src/okinawa.js
+++ src/okinawa.js
@@ -2,42 +2,56 @@ import Options from './options';
 import Logs from './logs';
 import Core from './core';
 import Device from './device';
+import Clock from './clocks/clock';
+import Localization from './localization/localization';
+
 import Effects from './effects/effects';
 import Particles from './effects/particles';
+import Emitter from './effects/emitter';
 import Sprites from './handlers/sprites';
 import Sounds from './handlers/sounds';
 import Fonts from './handlers/fonts';
-import Clock from './clocks/clock';
-import Localization from './localization/localization';
+
+import Scene from './scenes/scene';
+import Background from './scenes/background';
+import Preloader from './scenes/preloader';
+import Player from './player';
+import Item from './item';
+// import * as Font from '../lib/font.js/Font';
+
 import * as INPUT from './input/input';
 import * as GUI from './gui/gui';
 import * as MATH from './math/math';
-import Scenes from './scenes/scenes';
-import Preloader from './scenes/preloader';
-import Player from './player';
-import Engine from './engine';
-import Init from './init';
 
-let okinawa = {
+import engine from './engine';
+import init from './init';
+
+export {
   Options,
   Logs,
   Core,
   Device,
+  Clock,
+  Localization,
+  //
   Effects,
   Particles,
+  Emitter,
   Sprites,
   Sounds,
   Fonts,
-  Clock,
-  Localization,
-  Scenes,
+  //
+  Scene,
+  Background,
   Preloader,
   Player,
+  Item,
+  // Font,
+  //
   INPUT,
   GUI,
   MATH,
-  Engine,
-  Init
+  //
+  engine,
+  init
 };
-
-export default okinawa;
